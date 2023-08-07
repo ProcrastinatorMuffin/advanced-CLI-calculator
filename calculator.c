e310d4176b9cbd5738a9085ac82f05854aa90567
procrastinatorMuffin <danil.sholudko@icloud.com>
2023-08-07 20:50:04 +0900

    Fully changed the calculator logic. Now calculator can handle advanced mathematical equations. CLI interface was temporary deleted, because the project code was fully rewrited. CLI interface will back soon

--- calculator.c
+++ calculator.c
@@ -1,94 +1,202 @@
-// Include necessary libraries
-#include<stdio.h>
-#include<string.h>
-#include<math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
 
-// Function to display a list of commands and their explanations
-void showHelp() {
-    printf("\nAdvanced Calculator Commands:\n");
-    // List of commands with descriptions
-    printf("add [num1] [num2]         : Add two numbers\n");
-    printf("subtract [num1] [num2]    : Subtract second number from first\n");
-    printf("multiply [num1] [num2]    : Multiply two numbers\n");
-    printf("divide [num1] [num2]      : Divide first number by second\n");
-    printf("power [num] [exponent]    : Raise number to the power of exponent\n");
-    printf("sqrt [num]                : Calculate the square root of a number\n");
-    printf("sin [angle]               : Calculate the sine of an angle (in radians)\n");
-    printf("cos [angle]               : Calculate the cosine of an angle (in radians)\n");
-    printf("tan [angle]               : Calculate the tangent of an angle (in radians)\n");
-    printf("help                      : Show this help message\n");
-    printf("exit                      : Exit the calculator\n\n");
+
+#define PI 3.14159265
+
+typedef enum {
+    NUMBER,
+    OPERATOR,
+    FUNCTION
+} TokenType;
+
+typedef struct {
+    TokenType type;
+    union {
+        double value;
+        char op;
+        char func[10];
+    };
+} Token;
+
+typedef struct {
+    Token *data;
+    int size;
+    int top;
+} Stack;
+
+Stack* initStack(int size) {
+    Stack *stack = malloc(sizeof(Stack));
+    stack->data = malloc(size * sizeof(Token));
+    stack->size = size;
+    stack->top = -1;
+    return stack;
+}
+
+void push(Stack *stack, Token token) {
+    if (stack->top == stack->size - 1) {
+        stack->size *= 2;
+        stack->data = realloc(stack->data, stack->size * sizeof(Token));
+    }
+    stack->data[++stack->top] = token;
+}
+
+Token pop(Stack *stack) {
+    return stack->data[stack->top--];
+}
+
+Token peek(Stack *stack) {
+    return stack->data[stack->top];
+}
+
+int isStackEmpty(Stack *stack) {
+    return stack->top == -1;
+}
+
+int precedence(char op) {
+    switch (op) {
+        case '+':
+        case '-':
+            return 1;
+        case '*':
+        case '/':
+            return 2;
+        default:
+            return 0;
+    }
+}
+
+Token* tokenize(char *expression, int *length) {
+    Token *tokens = malloc(100 * sizeof(Token));
+    int tokenCount = 0;
+    for (int i = 0; i < strlen(expression); i++) {
+        if (expression[i] == ' ') {
+            continue;
+        } else if (isdigit(expression[i]) || expression[i] == '.') {
+            tokens[tokenCount].type = NUMBER;
+            sscanf(expression + i, "%lf", &(tokens[tokenCount].value));
+            while (isdigit(expression[i]) || expression[i] == '.') i++;
+            i--;
+        } else if (isalpha(expression[i])) {
+            tokens[tokenCount].type = FUNCTION;
+            int j = 0;
+            while (isalpha(expression[i])) {
+                tokens[tokenCount].func[j++] = expression[i++];
+            }
+            tokens[tokenCount].func[j] = '\0';
+            i--;
+        } else {
+            tokens[tokenCount].type = OPERATOR;
+            tokens[tokenCount].op = expression[i];
+        }
+        tokenCount++;
+    }
+    *length = tokenCount;
+    return tokens;
+}
+
+Token* shuntingYard(Token *tokens, int length, int *outputLength) {
+    Stack *operators = initStack(100);
+    Token *output = malloc(100 * sizeof(Token));
+    int outputCount = 0;
+
+    for (int i = 0; i < length; i++) {
+        if (tokens[i].type == NUMBER) {
+            output[outputCount++] = tokens[i];
+        } else if (tokens[i].type == FUNCTION) {
+            push(operators, tokens[i]);
+        } else if (tokens[i].type == OPERATOR) {
+            while (!isStackEmpty(operators) && precedence(peek(operators).op) >= precedence(tokens[i].op)) {
+                output[outputCount++] = pop(operators);
+            }
+            push(operators, tokens[i]);
+        } else if (tokens[i].op == '(') {
+            Token t;
+            t.type = OPERATOR;
+            t.op = '(';
+            push(operators, t);
+        } else if (tokens[i].op == ')') {
+            while (peek(operators).op != '(') {
+                output[outputCount++] = pop(operators);
+            }
+            pop(operators);
+        }
+    }
+
+    while (!isStackEmpty(operators)) {
+        output[outputCount++] = pop(operators);
+    }
+
+    *outputLength = outputCount;
+    return output;
+}
+
+double evaluate(Token *tokens, int length) {
+    Stack *values = initStack(100);
+
+    for (int i = 0; i < length; i++) {
+        if (tokens[i].type == NUMBER) {
+            push(values, tokens[i]);
+        } else if (tokens[i].type == OPERATOR) {
+            double b = pop(values).value;
+            double a = pop(values).value;
+            Token result;
+            result.type = NUMBER;
+            switch (tokens[i].op) {
+                case '+':
+                    result.value = a + b;
+                    break;
+                case '-':
+                    result.value = a - b;
+                    break;
+                case '*':
+                    result.value = a * b;
+                    break;
+                case '/':
+                    result.value = a / b;
+                    break;
+            }
+            push(values, result);
+        } else if (tokens[i].type == FUNCTION) {
+            double value = pop(values).value;
+            Token result;
+            result.type = NUMBER;
+            if (strcmp(tokens[i].func, "sin") == 0) {
+                result.value = sin(value * PI / 180.0);
+            } else if (strcmp(tokens[i].func, "cos") == 0) {
+                result.value = cos(value * PI / 180.0);
+            } else if (strcmp(tokens[i].func, "tan") == 0) {
+                result.value = tan(value * PI / 180.0);
+            } else if (strcmp(tokens[i].func, "sqrt") == 0) {
+                result.value = sqrt(value);
+            }
+            push(values, result);
+        }
+    }
+
+    return pop(values).value;
 }
 
 int main() {
-    // Variables to store the command and numbers for calculations
-    char command[50];
-    double num1, num2, result;
+    char expression[1000];
+    printf("Enter the expression: ");
+    fgets(expression, sizeof(expression), stdin);
+    expression[strcspn(expression, "\n")] = '\0';
 
-    // Initial message to the user
-    printf("Type 'help' for a list of commands.\n");
+    int length;
+    Token *tokens = tokenize(expression, &length);
+    int outputLength;
+    Token *postfixTokens = shuntingYard(tokens, length, &outputLength);
 
-    // Infinite loop to continuously prompt user for commands
-    while (1) {
-        printf("> ");  // Command prompt
-        scanf("%s", command);  // Read the command
+    double result = evaluate(postfixTokens, outputLength);
+    printf("Result: %lf\n", result);
 
-        // Check the entered command and perform the corresponding operation
-        if (strcmp(command, "exit") == 0) {
-            break;  // Exit the program
-        } else if (strcmp(command, "help") == 0) {
-            showHelp();  // Display the list of commands
-            continue;
-        } else if (strcmp(command, "add") == 0) {
-            scanf("%lf %lf", &num1, &num2);
-            result = num1 + num2;
-        } else if (strcmp(command, "subtract") == 0) {
-            scanf("%lf %lf", &num1, &num2);
-            result = num1 - num2;
-        } else if (strcmp(command, "multiply") == 0) {
-            scanf("%lf %lf", &num1, &num2);
-            result = num1 * num2;
-        } else if (strcmp(command, "divide") == 0) {
-            scanf("%lf %lf", &num1, &num2);
-            if (num2 == 0) {
-                printf("Error: Division by zero.\n");
-                continue;
-            }
-            result = num1 / num2;
-        } else if (strcmp(command, "power") == 0) {
-            scanf("%lf %lf", &num1, &num2);
-            result = pow(num1, num2);
-        } else if (strcmp(command, "sqrt") == 0) {
-            scanf("%lf", &num1);
-            if (num1 < 0) {
-                printf("Error: Negative number for square root.\n");
-                continue;
-            }
-            result = sqrt(num1);
-        } else if (strcmp(command, "sin") == 0) {
-            scanf("%lf", &num1);
-            result = sin(num1);
-        } else if (strcmp(command, "cos") == 0) {
-            scanf("%lf", &num1);
-            result = cos(num1);
-        } else if (strcmp(command, "tan") == 0) {
-            scanf("%lf", &num1);
-            if (cos(num1) == 0) {
-                printf("Error: Tangent undefined for this value.\n");
-                continue;
-            }
-            result = tan(num1);
-        } else {
-            printf("Invalid command. Type 'help' for a list of commands.\n");
-            continue;  // Go back to the start of the loop and prompt the user again
-        }
-
-        // Display the result of the calculation
-        printf("Result: %lf\n", result);
-    }
-
-    // Message displayed when the user exits the program
-    printf("Goodbye!\n");
+    free(tokens);
+    free(postfixTokens);
     return 0;
 }
+
 
